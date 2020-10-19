7b90b566c2a235a5f8181b85539126a598b74fba
Thieu Nguyen <thieu@bluegrassdigital.com>
2020-10-19 17:40:58 +0700

    Show History

--- src/components/HistoryModal/shared/HistoryItem.tsx
+++ src/components/HistoryModal/shared/HistoryItem.tsx
@@ -1,7 +1,47 @@
-import React, { FC, memo } from 'react';
-import styled from 'styled-components/native';
-import Fonts from '../../../theme/fonts';
-import Colors from '../../../theme/colors';
+import React, { FC, memo } from "react";
+import styled from "styled-components/native";
+import Fonts from "theme/fonts";
+import Colors from "theme/colors";
+
+const hidenCard = require("images/blue_back.png");
+const cards = [
+  require("images/1H.png"),
+  require("images/2H.png"),
+  require("images/3H.png"),
+  require("images/4H.png"),
+  require("images/5H.png"),
+  require("images/6H.png"),
+  require("images/7H.png"),
+  require("images/8H.png"),
+  require("images/9H.png"),
+  require("images/1D.png"),
+  require("images/2D.png"),
+  require("images/3D.png"),
+  require("images/4D.png"),
+  require("images/5D.png"),
+  require("images/6D.png"),
+  require("images/7D.png"),
+  require("images/8D.png"),
+  require("images/9D.png"),
+  require("images/1C.png"),
+  require("images/2C.png"),
+  require("images/3C.png"),
+  require("images/4C.png"),
+  require("images/5C.png"),
+  require("images/6C.png"),
+  require("images/7C.png"),
+  require("images/8C.png"),
+  require("images/9C.png"),
+  require("images/1S.png"),
+  require("images/2S.png"),
+  require("images/3S.png"),
+  require("images/4S.png"),
+  require("images/5S.png"),
+  require("images/6S.png"),
+  require("images/7S.png"),
+  require("images/8S.png"),
+  require("images/9S.png"),
+];
 
 interface Props {
   item: any;
@@ -25,14 +65,39 @@ const Text = styled(Fonts.Bold)`
 
 const ItemContainer = styled.View<StyledProps>`
   width: 100%;
-  background-color: ${Colors.SECOND_COLOR};
+  margin-vertical: 4px;
 `;
 
+const Image = styled.Image<StyledProps>`
+  width: ${(props) => props.width}px;
+  height: ${(props) => props.height}px;
+  resize-mode: contain;
+  align-self: center;
+  margin-horizontal: 2px;
+`;
+
+const View = styled.View`
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+`;
+
+const EmptyView = styled.View`
+  width: 28px;
+`;
 
 const HistoryItem: FC<Props> = ({ item }) => {
   return (
     <ItemContainer>
-      <Text>{item[0]}</Text>
+      <View>
+        <Image source={cards[item[0]]} width={40} height={60} />
+        <Image source={cards[item[1]]} width={40} height={60} />
+        <Image source={cards[item[2]]} width={40} height={60} />
+        <EmptyView />
+        <Image source={cards[item[3]]} width={40} height={60} />
+        <Image source={cards[item[4]]} width={40} height={60} />
+        <Image source={cards[item[5]]} width={40} height={60} />
+      </View>
     </ItemContainer>
   );
 };
